af9fdc6feb7843cd26603edac60546121828dbf6
Noah Gullo <noahgulloingram@gmail.com>
2026-06-22 15:32:45 -0700

    feat: split sections up into individual components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
 import { useState } from 'react'
 import './App.css'
 
+function GeneralInfo(){
+  return (
+    <>
+      <form>
+        <input></input>
+      </form>
+    </>
+  );
+}
+
+function Education(){
+  return (
+    <>
+      <p>HELLO WORLD</p>
+    </>
+  );
+}
+
+function Experience(){
+  return (
+    <>
+      <h2>Experience</h2>
+    </>
+  );
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <p>Hello world!</p>
+      <GeneralInfo />
+      <Education />
+      <Experience />
     </>
   );
 }
