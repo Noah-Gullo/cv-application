64f3dbf0e5fba49c0072ee2e46ea49277afff430
Noah Gullo <noahgulloingram@gmail.com>
2026-06-22 15:57:38 -0700

    mend

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import GeneralInfo from './components/GeneralInfo';
 import Education from './components/Education';
 import Experience from './components/Experience';
-import './App.css'
+import './styles/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
