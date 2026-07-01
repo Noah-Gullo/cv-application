f73e2bdaae7334a110c659e1ea8a1d70542d93ab
Noah Gullo <noahgulloingram@gmail.com>
2026-07-01 11:40:28 -0700

    feat: conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
+import { useState } from "react";
 import GeneralInfo from './components/GeneralInfo';
 import Education from './components/Education';
 import Experience from './components/Experience';
 import './styles/App.css'
 
-function App() {
+function App() {  
+  const [formVisible, setVisible] = useState(false);
+
+  function submitCV(){
+    setVisible(!formVisible);
+  }
+
+  if(formVisible){
+    return (
+      <>
+      <h1>test</h1>
+      </>
+    )
+  }
+
   return (
     <>
       <GeneralInfo />
       <Education />
       <Experience />
+      <button id="submitButton" onClick={submitCV}>Submit</button>
     </>
   );
 }
